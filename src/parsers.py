e7bc3de4b0e4c0bf2140e90177d4ac66c27b0456
Stephan Samuel <ssamuel@gmail.com>
2022-12-23 14:58:01 -0600

    Write trivial implementation of fn parser
    Doesn't work because functions refer to
    grouping functions, not individual calls
    within them. To be investigated.

--- src/parsers.py
+++ src/parsers.py
@@ -1,10 +1,15 @@
+import arcpy
 from wrapper import FunctionParser, ParsedFn
 
 class TrivialParser(FunctionParser):
 
     def parse_fn(self, name: str) -> ParsedFn:
-        #import arcpy
         #print(dir(arcpy))
+        def flt(fn: str) -> bool:
+            return not (fn.startswith("__") or fn.endswith("__"))
+        mock = [fn for fn in dir(arcpy) if flt(fn)]
+        if name in mock:
+            print(f"* found {name}")
 
         # This is a degenerate implementation; subclass.
         return ParsedFn(name)
