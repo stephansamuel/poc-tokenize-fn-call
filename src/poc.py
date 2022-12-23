3de0f17f7cbf4df18778073339fd13d6128f4a46
Stephan Samuel <ssamuel@gmail.com>
2022-12-23 13:12:21 -0600

    Add richer call signature to exercise options

--- src/poc.py
+++ src/poc.py
@@ -4,10 +4,10 @@ inst = wrapper.PocInstrumentation()
 inst.add_parser(wrapper.FunctionParser())
 
 @inst.fn_inst
-def i_am_a_function():
-    print("Hello, world!")
-    pass
+def i_am_a_function(a: str, b: str = None) -> str:
+    result: str = f"Hello, world! {a} {b}"
+    print(result)
+    return result
 
 if __name__ == "__main__":
-
-    i_am_a_function()
+    i_am_a_function("a_val", b="b_val")
