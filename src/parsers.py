93dd873f17ea0964aea5e5f5143ebf3e5446b1ed
Stephan Samuel <ssamuel@gmail.com>
2022-12-26 11:04:30 -0600

    Add drill into defined fns

--- src/parsers.py
+++ src/parsers.py
@@ -1,6 +1,8 @@
 import arcpy
+from functools import reduce
 import inspect
-from typing import List
+import sys
+from typing import List, Tuple
 from wrapper import FunctionParser, ParsedFn
 
 class TrivialParser(FunctionParser):
@@ -21,6 +23,8 @@ class TrivialParser(FunctionParser):
         print(code)
         '''
 
+        print(dict(sys.modules.copy()))
+
         # This is a degenerate implementation; subclass.
         return ParsedFn(name)
 
@@ -35,3 +39,14 @@ def get_fn_from_name(name: str) -> callable:
 def get_calls_from_fn(fn: callable) -> List[str]:
     code_lines = inspect.getsource(fn)
     return code_lines
+
+def get_names_of_all_loaded_modules() -> List[str]:
+    def flt(mod: str) -> bool:
+        return not mod.startswith("_")
+    all_modules = list(sys.modules.copy().keys())
+    all_modules.sort()
+    return [m for m in all_modules if flt(m)]
+
+def get_all_defined_functions() -> List[Tuple[str, object]]:
+    def red(mod: str):
+        pass
